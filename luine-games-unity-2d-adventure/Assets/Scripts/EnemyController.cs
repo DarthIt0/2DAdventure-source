0b7e2df7e98af45494a6dcd34ebb16fd84b20af2
DarthIt0 <luiley@protonmail.com>
2019-03-19 12:11:13 +0900

    luine

---
+++ luine-games-unity-2d-adventure/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public bool vertical;
+    Rigidbody2D rigidbody2D;
+    public float changeTime = 3.0f;
+    float timer;
+    int direction = 1;
+    Animator animator;
+    bool broken = true;
+    public ParticleSystem smokeEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
+        //smokeEffect = GetComponent<ParticleSystem>();
+        //smokeEffect.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+        Vector2 position = rigidbody2D.position;
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed*direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed*direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+        }
+
+        rigidbody2D.MovePosition(position);
+
+        if (!broken) {
+            return;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if(player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
+    }
+}
